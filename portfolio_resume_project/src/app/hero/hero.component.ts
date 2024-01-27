48ce23c18b461cf6cd4f7362c692488e194634a6
lorenz <renzlendio@protonmail.com>
2024-01-28 00:32:09 +0800

    Finished Hero

--- portfolio_resume_project/src/app/hero/hero.component.ts
+++ portfolio_resume_project/src/app/hero/hero.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnDestroy, OnInit, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
 
+  words = [
+    "Freelancer",
+    "Data Analyst",
+    "Web Developer"
+  ]
+  typeSpeed = 100;
+  deleteSpeed = 80;
+
+  wordIndex = signal(0);
+  word = computed(() => this.words[this.wordIndex()]);
+
+  textIndex = signal(0);
+  text = computed(() => this.word().slice(0, this.textIndex()));
+
+  showTitles = false;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.type();
+  }
+
+  type(): void {
+    const int = setInterval(() => {
+      this.textIndex.update((v) => v + 1);
+      if (this.textIndex() > this.word().length) {
+        clearInterval(int);
+        this.delete();
+      }
+    }, this.typeSpeed);
+  }
+
+  delete(): void {
+    const int = setInterval(() => {
+      this.textIndex.update((v) => v - 1);
+      if (this.textIndex() < 0) {
+        this.wordIndex.update((v) => (v + 1) % this.words.length);
+        clearInterval(int);
+        this.textIndex.set(0);
+        this.type();
+      }
+    }, this.deleteSpeed);
+  }
 }
