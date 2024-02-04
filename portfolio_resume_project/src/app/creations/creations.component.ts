aa6abba4a8ba1b0b39689c605d22a4308e5c75ee
lorenz <renzlendio@protonmail.com>
2024-02-04 17:40:15 +0800

    Finished Task

--- portfolio_resume_project/src/app/creations/creations.component.ts
+++ portfolio_resume_project/src/app/creations/creations.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+import { creationsData } from './creationsData';
 
 @Component({
   selector: 'app-creations',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './creations.component.html',
-  styleUrl: './creations.component.css'
+  styleUrl: './creations.component.css',
 })
-export class CreationsComponent {
+export class CreationsComponent implements OnInit {
+  creations: any;
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.creations = creationsData;
+  }
 }
